1ac2315f4f13cbfbf721ee07c456b7b738251c32
ShihPing0803 <119124535+loading2022@users.noreply.github.com>
2024-02-24 00:53:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -19,20 +19,44 @@ handler = WebhookHandler(channel_secret)
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    signature = request.headers['X-Line-Signature']
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+        body = request.get_data(as_text=True)
+    json_data = json.loads(body)
+    print(json_data)
     try:
+        line_bot_api = LineBotApi('hcdps8AlQyoTQswQTh4CfEbvaQOR2h1w5XNQuNrifdUlYHL21npOTOLDDJVKaBW1ppI53vAyeS539ai8wfayu9azie35qiyFp27NxbS4JoYCcONUQAKoyBgl5TyeQWHUdypDgAmbvf8qyyxdtQA3ZwdB04t89/1O/w1cDnyilFU=')
+        handler = WebhookHandler('4d0c4342dd19335b15bca01e32944a34')
+        signature = request.headers['X-Line-Signature']
         handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
+        tk = json_data['events'][0]['replyToken']
+        msg = json_data['events'][0]['message']['text']
+        # 取出文字的前五個字元，轉換成小寫
+        ai_msg = msg[:6].lower()
+        reply_msg = ''
+        # 取出文字的前五個字元是 hi ai:
+        if ai_msg == 'hi ai:':
+            openai.api_key = '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'
+            # 將第六個字元之後的訊息發送給 OpenAI
+            response = openai.Completion.create(
+                model='text-davinci-003',
+                prompt=msg[6:],
+                max_tokens=256,
+                temperature=0.5,
+                )
+            # 接收到回覆訊息後，移除換行符號
+            reply_msg = response["choices"][0]["text"].replace('\n','')
+        else:
+            reply_msg = msg
+        text_message = TextSendMessage(text=reply_msg)
+        line_bot_api.reply_message(tk,text_message)
+    except:
+        print('error')
     return 'OK'
-
+"""
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
-
+"""
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
