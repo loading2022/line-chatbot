d7a65d3c264094b531a4189046063ad139fe9ab4
ShihPing0803 <119124535+loading2022@users.noreply.github.com>
2024-02-24 21:43:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -20,15 +20,12 @@ handler = WebhookHandler(channel_secret)
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
+    json_data = json.loads(body)
+    print(json_data)               # 印出 json_data
     return 'OK'
 
+"""
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
@@ -57,6 +54,7 @@ def handle_file_message(event):
     #line_bot_api.reply_message(event.reply_token, TextSendMessage(text="File uploaded successfully."))
 
 import os
+"""
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
